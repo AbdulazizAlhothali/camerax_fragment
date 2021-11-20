1a9ca82a446d2924a72485d4c9932b8a4f3f697d
azoz1 <Abdulaziz.Alhothali.jed.k1@tuwaiq.edu.sa>
2021-11-20 22:33:12 +0300

    trying mvvm

--- app/src/main/java/com/tuwaiq/cameraxfragment/ui/main/MainFragment.kt
+++ app/src/main/java/com/tuwaiq/cameraxfragment/ui/main/MainFragment.kt
@@ -17,9 +17,15 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.tuwaiq.cameraxfragment.R
+import com.tuwaiq.cameraxfragment.databinding.MainFragmentBinding
 import kotlinx.android.synthetic.main.main_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 import java.io.File
 import java.nio.ByteBuffer
@@ -36,17 +42,18 @@ class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
-        private const val TAG = "CameraXBasic"
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        const val TAG = "CameraXBasic"
+        const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
     }
 
-    private lateinit var captureBtn: Button
+    private lateinit var viewModel: MainViewModel
+    //private lateinit var captureBtn: Button
     private lateinit var cameraView: PreviewView
     private lateinit var safeContext: Context
     private var imageCapture: ImageCapture? = null
-    private lateinit var outputDirectory: File
+    //private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
 
     override fun onAttach(context: Context) {
@@ -60,13 +67,22 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        //return inflater.inflate(R.layout.main_fragment, container, false)
+        val binding: MainFragmentBinding= DataBindingUtil.inflate(
+            inflater,R.layout.main_fragment, container, false
+        )
+
+        viewModel= ViewModelProviders.of(this)[MainViewModel::class.java]
+
+        binding.lifecycleOwner= this
+        binding.viewModel = viewModel
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        captureBtn= view.findViewById(R.id.camera_capture_button)
+        //captureBtn= view.findViewById(R.id.camera_capture_button)
         cameraView= view.findViewById(R.id.viewFinder)
 
         // Request camera permissions
@@ -78,52 +94,56 @@ class MainFragment : Fragment() {
         }
 
         // Set up the listener for take photo button
-        captureBtn.setOnClickListener {
+        /*captureBtn.setOnClickListener {
             takePhoto()
-            /*val i = Intent(this, ImagePreview::class.java)
-            i.putExtra("photo", )*/
-        }
+            *//*val i = Intent(this, ImagePreview::class.java)
+            i.putExtra("photo", )*//*
+        }*/
 
-        outputDirectory = getOutputDirectory()
+        //outputDirectory = getOutputDirectory()
 
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
-    private fun takePhoto() {
-        // Get a stable reference of the modifiable image capture use case
-        val imageCapture = imageCapture ?: return
+   /* private fun takePhoto() {
+        MainScope().launch(Dispatchers.Main) {
+            // Get a stable reference of the modifiable image capture use case
+            val imageCapture = imageCapture ?: return@launch
 
-        // Create time-stamped output file to hold the image
-        val photoFile = File(
-            outputDirectory,
-            SimpleDateFormat(FILENAME_FORMAT, Locale.US
-            ).format(System.currentTimeMillis()) + ".jpg")
+            // Create time-stamped output file to hold the image
+            val photoFile = File(
+                outputDirectory,
+                SimpleDateFormat(FILENAME_FORMAT, Locale.US
+                ).format(System.currentTimeMillis()) + ".jpg")
 
-        // Create output options object which contains file + metadata
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
-
-
-        // Set up image capture listener, which is triggered after photo has
-        // been taken
-        imageCapture.takePicture(
-            outputOptions, ContextCompat.getMainExecutor(safeContext), object : ImageCapture.OnImageSavedCallback {
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
-                }
-
-                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    val savedUri = Uri.fromFile(photoFile)
-                    val msg = "Photo capture succeeded: $savedUri"
-                    Toast.makeText(safeContext, msg, Toast.LENGTH_SHORT).show()
-                    Log.d(TAG, msg)
-                    /*val i = Intent(this@MainActivity, ImagePreview::class.java)
-                    i.putExtra("data",savedUri)
-                    startActivity(i)*/
-                }
-            })
+            // Create output options object which contains file + metadata
+            val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
 
 
-    }
+            // Set up image capture listener, which is triggered after photo has
+            // been taken
+            imageCapture.takePicture(
+                outputOptions, ContextCompat.getMainExecutor(safeContext), object : ImageCapture.OnImageSavedCallback {
+                    override fun onError(exc: ImageCaptureException) {
+                        Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                    }
+
+                    override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                        val savedUri = Uri.fromFile(photoFile)
+                        val msg = "Photo capture succeeded: $savedUri"
+                        Toast.makeText(safeContext, msg, Toast.LENGTH_SHORT).show()
+                        Log.d(TAG, msg)
+                        *//*val i = Intent(this@MainActivity, ImagePreview::class.java)
+                        i.putExtra("data",savedUri)
+                        startActivity(i)*//*
+                    }
+                })
+
+        }
+
+
+
+    }*/
 
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(safeContext)
--- app/src/main/java/com/tuwaiq/cameraxfragment/ui/main/MainViewModel.kt
+++ app/src/main/java/com/tuwaiq/cameraxfragment/ui/main/MainViewModel.kt
@@ -1,7 +1,78 @@
 package com.tuwaiq.cameraxfragment.ui.main
 
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+    //val captureBtn: MutableLiveData<Button> = MutableLiveData(binding)
+    //var cameraView: PreviewView
+
+
+    @SuppressLint("StaticFieldLeak")
+    private val context = getApplication<Application>().applicationContext
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
+
+    fun takePhoto() {
+        viewModelScope.launch(Dispatchers.Main) {
+            // Get a stable reference of the modifiable image capture use case
+            val imageCapture = imageCapture ?: return@launch
+
+            // Create time-stamped output file to hold the image
+            val photoFile = File(
+                outputDirectory,
+                SimpleDateFormat(
+                    MainFragment.FILENAME_FORMAT, Locale.US
+                ).format(System.currentTimeMillis()) + ".jpg")
+
+            // Create output options object which contains file + metadata
+            val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+
+            // Set up image capture listener, which is triggered after photo has
+            // been taken
+            imageCapture.takePicture(
+                outputOptions, ContextCompat.getMainExecutor(context), object : ImageCapture.OnImageSavedCallback {
+                    override fun onError(exc: ImageCaptureException) {
+                        Log.e(MainFragment.TAG, "Photo capture failed: ${exc.message}", exc)
+                    }
+
+                    override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                        val savedUri = Uri.fromFile(photoFile)
+                        val msg = "Photo capture succeeded: $savedUri"
+                        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+                        Log.d(MainFragment.TAG, msg)
+                        /*val i = Intent(this@MainActivity, ImagePreview::class.java)
+                        i.putExtra("data",savedUri)
+                        startActivity(i)*/
+                    }
+                })
+
+        }
+
+
+
+    }
+
+
 }
